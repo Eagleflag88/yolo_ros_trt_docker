f9d1b8b22501dbbf2c8c98c74878cedfac4f9002
Eagleflag88 <yijiang.xie@foxmail.com>
2022-05-04 01:40:48 +0800

    Modify Dockerfile and Delete yolov5.engine

--- docker/build.sh
+++ docker/build.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
+###
+ # @Author: Eagleflag88 yijiang.xie@foxmail.com
+ # @Date: 2022-05-03 15:50:05
+ # @LastEditors: Eagleflag88 yijiang.xie@foxmail.com
+ # @LastEditTime: 2022-05-04 01:18:57
+ # @FilePath: /yolo_ros_trt_docker/docker/build.sh
+ # @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+### 
 docker build --tag yolov5_trt -f Dockerfile .
 
