7ee37cbc1b0cfdfba2ae357a1d244eddba3cf1e8
Eagleflag88 <yijiang.xie@foxmail.com>
2022-05-06 18:43:01 +0800

    Add hrnet library to deepsort node

--- docker/run.sh
+++ docker/run.sh
@@ -3,7 +3,7 @@
  # @Author: Eagleflag88 yijiang.xie@foxmail.com
  # @Date: 2022-05-03 15:50:05
  # @LastEditors: Eagleflag88 yijiang.xie@foxmail.com
- # @LastEditTime: 2022-05-06 11:55:52
+ # @LastEditTime: 2022-05-06 18:29:36
  # @FilePath: /docker/run.sh
  # @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 ### 
